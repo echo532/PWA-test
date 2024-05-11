c15b60603418744ab99f3ff9cf7d6fb293ed8d11
echo532 <84986609+echo532@users.noreply.github.com>
2024-05-11 15:43:21 -0400

    PWA base setup

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://echo532.github.io/PWA-test/",
+  base: "/PWA-test/",
 })
